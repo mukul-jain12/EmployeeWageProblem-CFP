dc62c62760545c80ab3578b667993bdfa6be73e4
Mukul Jain <mukuljain422@gmail.com>
2021-12-15 09:12:12 +0530

    checking employee attendance

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,10 @@
 """Welcome To Employee Wage Problem"""
+import random
+
+empCheck = random.randrange(0, 2)
+
+# checking attendance
+if empCheck == 0:
+    print("Present")
+else:
+    print("Absent")
