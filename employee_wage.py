c9bd4aadcd561539bd2b4e51c2f43e435646216f
Mukul Jain <mukuljain422@gmail.com>
2021-12-15 11:19:17 +0530

    calculate emp wage till working days or 100 hours

--- employee_wage.py
+++ employee_wage.py
@@ -4,14 +4,16 @@ import random
 EMP_RATE_PER_HOUR = 20
 FULL_TIME = 8
 PART_TIME = 4
+WORKING_HOURS = 100
 
-WORKING_DAYS = 20
+working_days = 0
 total_emp_hour = 0
 
-for day in range(1, WORKING_DAYS+1):
+while working_days < 20 and total_emp_hour <= WORKING_HOURS:
+    working_days += 1
     emp_check = random.randrange(0, 3)
 
-    # checking attendance
+    # checking full-timme, part-time or absent
     if emp_check == 0:
         emp_hour = FULL_TIME
     elif emp_check == 1:
@@ -20,6 +22,10 @@ for day in range(1, WORKING_DAYS+1):
         emp_hour = 0
 
     total_emp_hour += emp_hour
+
+    if total_emp_hour > 100:
+        total_emp_hour -= emp_hour
+        break
 
 # calculating monthly wage of employee
 emp_wage = EMP_RATE_PER_HOUR * total_emp_hour
