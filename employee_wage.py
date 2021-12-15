e929ed9761a7fd48a6562e1a4f2b3ec29ccdbf15
Mukul Jain <mukuljain422@gmail.com>
2021-12-15 09:22:17 +0530

    calculating employee daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,10 +1,17 @@
 """Welcome To Employee Wage Problem"""
 import random
 
-empCheck = random.randrange(0, 2)
+EMP_RATE_PER_HOUR = 20
+FULL_TIME = 8
+
+emp_check = random.randrange(0, 2)
 
 # checking attendance
-if empCheck == 0:
-    print("Present")
+if emp_check == 0:
+    emp_hour = FULL_TIME
 else:
-    print("Absent")
+    emp_hour = 0
+
+# calculating employee wage
+emp_wage = EMP_RATE_PER_HOUR * emp_hour
+print(f"Monthly Wage of a Employee is :{emp_wage}")
